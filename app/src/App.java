f18ce2f7670e374bb0f0824f19434d098402c805
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2022-11-25 16:58:45 -0300

    Teste faltando completar

--- app/src/App.java
+++ app/src/App.java
@@ -4,10 +4,26 @@ import java.util.List;
 //import org.json.simple.*;;
 
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Produto prod1 = new Produto("1", "Cachorro quente", 2.50F);
         Produto prod2 = new Produto("2", "Coca", 0.50F);
 
+
+
+
+
+
+
+
+
+
+
+        /////////// METODOS -FASE DE TESTE- ///////////
+        
         List<Produto> pedidos = new ArrayList<Produto>();
         pedidos.add(prod2);
         pedidos.add(prod1);
